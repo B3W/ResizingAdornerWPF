e509ac642666884f93eef769bfc76ccf5c3e03c2
B3W <westieberg@msn.com>
2025-07-20 21:17:37 -0500

    Added coordinate caching for thumbs. Reduces allocations and number of calculations per ArrangeOverride call.

--- ResizablePanel/ResizeAdorner.cs
+++ ResizablePanel/ResizeAdorner.cs
@@ -94,6 +94,11 @@ namespace ResizablePanel
       /// </summary>
       private readonly double _minHeight;
 
+      /// <summary>
+      /// Cache that holds coordinates for resizing thumbs
+      /// </summary>
+      private readonly Rect[] _thumbCoordinates;
+
       #endregion // Fields
 
 
@@ -102,47 +107,6 @@ namespace ResizablePanel
       // Overriding FrameworkElement property as this adorner will contain more than one visual
       // Allows WPF framework to interface with this adorner's visual collection
       protected override int VisualChildrenCount => _visualChildren.Count;
-
-
-      private Rect TopThumbRect => new Rect((DesiredSize.Width / 2.0D) - (_config.ThumbThickness / 2),
-                                            -(_config.ThumbThickness / 2),
-                                            _config.ThumbThickness,
-                                            _config.ThumbThickness);
-
-      private Rect BottomThumbRect => new Rect((DesiredSize.Width / 2.0D) - (_config.ThumbThickness / 2),
-                                               DesiredSize.Height - (_config.ThumbThickness / 2),
-                                               _config.ThumbThickness,
-                                               _config.ThumbThickness);
-
-      private Rect LeftThumbRect => new Rect(-(_config.ThumbThickness / 2),
-                                             (DesiredSize.Height / 2.0D) - (_config.ThumbThickness / 2),
-                                             _config.ThumbThickness,
-                                             _config.ThumbThickness);
-
-      private Rect RightThumbRect => new Rect(DesiredSize.Width - (_config.ThumbThickness / 2),
-                                              (DesiredSize.Height / 2.0D) - (_config.ThumbThickness / 2),
-                                              _config.ThumbThickness,
-                                              _config.ThumbThickness);
-
-      private Rect TopLeftThumbRect => new Rect(-(_config.ThumbThickness / 2),
-                                                -(_config.ThumbThickness / 2),
-                                                _config.ThumbThickness,
-                                                _config.ThumbThickness);
-
-      private Rect TopRightThumbRect => new Rect(DesiredSize.Width - (_config.ThumbThickness / 2),
-                                                 -(_config.ThumbThickness / 2),
-                                                 _config.ThumbThickness,
-                                                 _config.ThumbThickness);
-
-      private Rect BottomLeftThumbRect => new Rect(-(_config.ThumbThickness / 2),
-                                                   DesiredSize.Height - (_config.ThumbThickness / 2),
-                                                   _config.ThumbThickness,
-                                                   _config.ThumbThickness);
-
-      private Rect BottomRightThumbRect => new Rect(DesiredSize.Width - (_config.ThumbThickness / 2),
-                                                    DesiredSize.Height - (_config.ThumbThickness / 2),
-                                                    _config.ThumbThickness,
-                                                    _config.ThumbThickness);
 
       #endregion // Properties
 
@@ -184,6 +148,19 @@ namespace ResizablePanel
             _ = _visualChildren.Add(thumb);
          }
 
+         // Create starting coordinates for all thumbs that will be updated in ArrangeOverride
+         // Most thumbs only need one coordinate change during arrange pass since they are arranged relative to the adorned control
+         _thumbCoordinates = new Rect[(int)ThumbIndex.NumThumbs];
+
+         _thumbCoordinates[(int)ThumbIndex.Top]         = new Rect(                         0.0D, -(_config.ThumbThickness / 2), _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.Bottom]      = new Rect(                         0.0D,                          0.0D, _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.Left]        = new Rect(-(_config.ThumbThickness / 2),                          0.0D, _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.Right]       = new Rect(                         0.0D,                          0.0D, _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.TopLeft]     = new Rect(-(_config.ThumbThickness / 2), -(_config.ThumbThickness / 2), _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.TopRight]    = new Rect(                         0.0D, -(_config.ThumbThickness / 2), _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.BottomLeft]  = new Rect(-(_config.ThumbThickness / 2),                          0.0D, _config.ThumbThickness, _config.ThumbThickness);
+         _thumbCoordinates[(int)ThumbIndex.BottomRight] = new Rect(                         0.0D,                          0.0D, _config.ThumbThickness, _config.ThumbThickness);
+
          // Set the minimum dimensions for the adorned element
          _minWidth = _adornedFrameworkElement.MinWidth.Equals(double.NaN) ? _config.ThumbThickness * 3
                                                                           : _adornedFrameworkElement.MinWidth;
@@ -197,14 +174,29 @@ namespace ResizablePanel
       // Overriding FrameworkElement method to place adorners in correct location
       protected override Size ArrangeOverride(Size finalSize)
       {
-         _resizeThumbs[(int)ThumbIndex.Top].Arrange(TopThumbRect);
-         _resizeThumbs[(int)ThumbIndex.Bottom].Arrange(BottomThumbRect);
-         _resizeThumbs[(int)ThumbIndex.Left].Arrange(LeftThumbRect);
-         _resizeThumbs[(int)ThumbIndex.Right].Arrange(RightThumbRect);
-         _resizeThumbs[(int)ThumbIndex.TopLeft].Arrange(TopLeftThumbRect);
-         _resizeThumbs[(int)ThumbIndex.TopRight].Arrange(TopRightThumbRect);
-         _resizeThumbs[(int)ThumbIndex.BottomLeft].Arrange(BottomLeftThumbRect);
-         _resizeThumbs[(int)ThumbIndex.BottomRight].Arrange(BottomRightThumbRect);
+         // Calculate new coordinates for thumbs
+         // NOTE: Use 'finalSize' instead of 'DesiredSize'. 'DesiredSize' does not behave well with zooming
+         _thumbCoordinates[(int)ThumbIndex.Top].X = (finalSize.Width / 2.0D) - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.Bottom].X = (finalSize.Width / 2.0D) - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.Bottom].Y = finalSize.Height - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.Left].Y = (finalSize.Height / 2.0D) - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.Right].X = finalSize.Width - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.Right].Y = (finalSize.Height / 2.0D) - (_config.ThumbThickness / 2);
+         //_thumbCoordinates[(int)ThumbIndex.TopLeft] -> No Change
+         _thumbCoordinates[(int)ThumbIndex.TopRight].X = finalSize.Width - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.BottomLeft].Y = finalSize.Height - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.BottomRight].X = finalSize.Width - (_config.ThumbThickness / 2);
+         _thumbCoordinates[(int)ThumbIndex.BottomRight].Y = finalSize.Height - (_config.ThumbThickness / 2);
+
+         // Apply new coordinates to thumbs
+         _resizeThumbs[(int)ThumbIndex.Top].Arrange(_thumbCoordinates[(int)ThumbIndex.Top]);
+         _resizeThumbs[(int)ThumbIndex.Bottom].Arrange(_thumbCoordinates[(int)ThumbIndex.Bottom]);
+         _resizeThumbs[(int)ThumbIndex.Left].Arrange(_thumbCoordinates[(int)ThumbIndex.Left]);
+         _resizeThumbs[(int)ThumbIndex.Right].Arrange(_thumbCoordinates[(int)ThumbIndex.Right]);
+         _resizeThumbs[(int)ThumbIndex.TopLeft].Arrange(_thumbCoordinates[(int)ThumbIndex.TopLeft]);
+         _resizeThumbs[(int)ThumbIndex.TopRight].Arrange(_thumbCoordinates[(int)ThumbIndex.TopRight]);
+         _resizeThumbs[(int)ThumbIndex.BottomLeft].Arrange(_thumbCoordinates[(int)ThumbIndex.BottomLeft]);
+         _resizeThumbs[(int)ThumbIndex.BottomRight].Arrange(_thumbCoordinates[(int)ThumbIndex.BottomRight]);
 
          return finalSize;
       }
